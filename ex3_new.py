9327a4c7535e5c92dd0b7217f7fde07953b92e51
Jessica.E.Spencer <jessicaspencer12@gmail.com>
2019-02-26 17:58:45 -0500

    ex3 study drill 3

---
+++ ex3_new.py
@@ -0,0 +1,7 @@
+#!usr/bin/env python2.7
+
+print '''I want to calculate my monthly expenses: \n Rent \t 700.00 \n Metro \t 25.00 \n Gas \t 33.00 \n Water \t 28.00 \n Electric \t 28.00
+
+Dinner_Out \t 25.00
+'''
+print "Total", 700.00 + 25.00 + 33.00 + 28.00
