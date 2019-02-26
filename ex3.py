fac863856aff31e5ebdacb538d28365be2734ba5
Jessica.E.Spencer <jessicaspencer12@gmail.com>
2019-02-26 15:15:59 -0500

    Renamed Ex1.py

---
+++ ex3.py
@@ -0,0 +1,35 @@
+# !usr/bin/env python2.7
+
+#ths line prints the statement about couting chickens
+print "I will now count my chickens:"
+#this line prints 'Hens'and then it does math
+print "Hens", 25 + 30 / 6
+#this line prints and then does math
+print "Roosters", 100 - 25 * 3 % 4
+#this line is a print statement
+print "Now I will count the eggs:"
+#this line does math
+print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 +6
+#this prints a statement
+print "Is it true that 3 + 2 < 5 -7 ?"
+#this line does math
+print 3 + 2 < 5 - 7
+#this line prints numbers in quotes and does math
+print "What is 3 + 2?", 3 + 2
+#this line prints a statement then does math
+print "What is 5 - 7?", 5 - 7
+#this line prints a statement
+print "Oh, that's why it's False."
+#this line prints is a statement
+print "How about some more."
+#print statement plus greater than or equals answer True or false
+print "Is it greater?", 5 > -2
+#print statement plus greater
+print "Is it greater or equal?", 5 >= -2
+#print question which is a print statement plus false
+print "Is it less or equal?", 5 < = -2
+
+'''
+study Drills
+use a '#' above each line to explain what the line does
+'''
