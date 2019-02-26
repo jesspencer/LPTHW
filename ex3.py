99d244c267046efc25e4c4174f656adb4c7bfc02
Jessica.E.Spencer <jessicaspencer12@gmail.com>
2019-02-26 17:58:00 -0500

    Updated print statements to floating point

--- ex3.py
+++ ex3.py
@@ -33,4 +33,28 @@ print "Is it less or equal?", 5 < = -2
 study Drills
 use a '#' above each line to explain what the line does
 
+find something you need to calculate adn write a new .py file that does it
+
+research floating point ... done
+
+rewrite ex3.py to use floating point numbers
+
+float point statements I use .00 but i could have just used .0 
+
 '''
+
+print "writing ex3 to incldue floating point #'s "
+
+print " I will now count my chickens: "
+
+print "Hens", 25.00 + 30.00 / 6.00
+
+print "Roosters", 100.00 - 25.00 * 3.00 % 4.00
+
+print "Now I will count the eggs:"
+
+print 3.00 + 2.00  + 1.00 - 5.00 + 4.00 % 2.00 - 1.00 / 4.00 + 6.00
+
+print 3.00 2.00 < 5.00 - 7.00
+
+print "What is 3.00 + 2.00?", 3.00 + 2.00
