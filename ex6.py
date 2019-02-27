36256d04391f36806fd08c7d0e5d858107f4cb41
Jessica.E.Spencer <jessicaspencer12@gmail.com>
2019-02-26 22:59:07 -0500

    Updated file to explain what each line means

--- ex6.py
+++ ex6.py
@@ -1,23 +1,29 @@
 #!usr/bin/env python2.7
-
+#this is a variable x
 x = "There are %d types of people." % 10
+#this is a varibale binary
 binary = "binary"
+#this is a variable do_not"
 do_not = "don't"
+#this is a variable y
 y = "Those who know %s and those who %s." % (binary, do_not)
-
+#print statement
 print x
+#print statement this prints the variable y
 print y
-
+#print statement for x
 print "I said : %r." % x
+#this is the print satement that includes the variable y
 print "I also said: '%s'." % y
-
+#variabel hilarious
 hilarious = False
-
+#variable joke_evaluation
 joke_evaluation = "Isn't tht joke so funny?! %r"
-
+#prints false
 print joke_evaluation % hilarious
-
+#creating variable w
 w = "This is the left side of ..."
+#creates the variable e
 e = "a string with a right side."
-
+#prints the variable w along with e and the plus sign provides spacing 
 print w + e
