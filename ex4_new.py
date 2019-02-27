1696d819b9efe62e8ebe0057ee3d6cd9afa95bb4
Jessica.E.Spencer <jessicaspencer12@gmail.com>
2019-02-26 22:12:28 -0500

    Calculated ingredients for fruit salad

---
+++ ex4_new.py
@@ -0,0 +1,11 @@
+#!usr/bin/env python2.7
+
+apples = 2
+bananas = 1
+mayo = 1
+bowl_of_fruit = apples + bananas
+ingredients_count = bowl_of_fruit + mayo
+
+print "I have this many apples", apples
+print "I have this many bananas", bananas
+print "I will need this many ingredients for a fruit salad", ingredients_count
