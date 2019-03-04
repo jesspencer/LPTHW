da4389e2dd2544890c75416f9f024f381f7d6dc2
Jessica.E.Spencer <jessicaspencer12@gmail.com>
2019-03-03 23:37:03 -0500

    Updated file with exercise 7

---
+++ ex7.py
@@ -0,0 +1,27 @@
+#!usr/bin/env python2
+
+# More Printing
+
+print "Mary had a little lamb." # print statement
+print "It's fleece was whit as %s." % 'snow' # the %s inputs the word snow
+print "And everywhere that Mary went." # general print statement
+print "." * 10 #what did that do? # this line '.' write ten elispes
+
+end1= "C" # variable
+end2 = "h" # variable
+end3 = "e" # variable
+end4 = "e" # variable
+end5 = "s" # variable
+end6 = "e" # variable
+end7 = "B" # variable
+end8 = "u" # variable
+end9 = "r" # variable
+end10 = "g" # variable
+end11 = "e" # variable
+end12 = "r" # variable
+
+#try removing the comma at the end adn see what happens
+print end1 + end2 + end3 + end4 + end5 + end6, # this line prints with the variables
+print end7 + end8 + end9 + end10 + end11 + end12 # this line prints the variables
+
+#go back and write what each line does 
