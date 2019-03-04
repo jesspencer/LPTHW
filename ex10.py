770b7e200c04daf9914b7d9384e4e01764bec9ca
Jessica.E.Spencer <jessicaspencer12@gmail.com>
2019-03-04 00:58:02 -0500

    Added exercise 10 along with study drill comments above each line

---
+++ ex10.py
@@ -0,0 +1,17 @@
+#!usr/bin/env Python2
+
+tabby_cat = "\tI'm tabbed in." # variable 'I'm tabbed in
+persian_cat = "I'm split\non a line" # the \n creates a new line
+backslash_cat = "I'm \\ a \\ cat." # the \\ creates a single slash
+
+fat_cat = """
+I'll do a list:
+\t* Cat food
+\t* Fishies
+\t* Catnip\n\t* Grass
+""" # fat_cat is a variable && \t creates a tab
+
+print tabby_cat # regular print statement
+print persian_cat # regular print statement
+print backslash_cat # regular print statement
+print fat_cat # regular print statement 
