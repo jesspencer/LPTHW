f127da9f2b79c65b37ef6afb1f61a67a5b6bccfd
Jessica.E.Spencer <jessicaspencer12@gmail.com>
2019-03-04 00:39:29 -0500

    Added exercise 9 to file

---
+++ ex9.py
@@ -0,0 +1,14 @@
+#!usr/bin/env python2.7
+
+days = "Mon Tues Wed Thu Fri Sat Sun" # variable
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug" #variable with new lines between months
+
+print "Here are the days: ", days # print statement & variable
+print "Here are the months", months # print statement & variable
+# below are print statemnets
+print """
+There's somthing going on here.
+With the three double-quotes.
+We'll be able to type as much as we like.
+Even 4 lines if we want, or 5, or 6.
+"""
