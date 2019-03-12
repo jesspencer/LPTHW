3dea29015132faa4aa705fe21942e1e4201eb5da
Jessica.E.Spencer <jessicaspencer12@gmail.com>
2019-03-12 19:23:27 -0400

    feat:Updated the code from ex11.py typing the same sentences but with fewer words

---
+++ ex12.py
@@ -0,0 +1,7 @@
+#!usr/bin/env python2.7
+
+age = raw_input("How old are you?")
+height = raw_input("How tall are you?")
+weight = raw_input("How much do you weigh?")
+
+print "So you are %r years old, you are %r tall, and you weigh %r pounds." % (age, height, weight)
