4e5acff2bba0b9a17d5a7f5869e5e4c432823ed0
Jessica.E.Spencer <jessicaspencer12@gmail.com>
2019-03-13 11:11:19 -0400

    style:Deleted the six lines of code for writing the sentences and replaced it with '%r' to shorten the code

--- ex16.py
+++ ex16.py
@@ -25,12 +25,15 @@ line3 = raw_input("line 3: ")
 
 print "I'm going to write theese to the file."
 
-target.write(line1)
-target.write("\n")
-target.write(line2)
-target.write("\n")
-target.write(line3)
-target.write("\n")
+target.write("%r \n %r \n %r ") % (line1, line2, line3)
 
 print "And finally, we close it."
 target.close()
+
+"""
+Study Drills
+
+3. There is too much repetition in this file. Use strings, formats, and escapes to
+print out line1, line2, and line3 with just one target.write() command instead
+of six.
+"""
