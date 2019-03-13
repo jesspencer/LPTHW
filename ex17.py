b793a1b93268a5a1387895b94259c70f34e1240d
Jessica.E.Spencer <jessicaspencer12@gmail.com>
2019-03-13 12:15:23 -0400

    feat:Made the code shorter by removing the file functions showing the length of the file and the message about aborting the file

--- ex17.py
+++ ex17.py
@@ -8,14 +8,9 @@ script, from_file, to_file = argv
 print "Copying from %s to %s" % (from_file, to_file)
 
 #we could do thse two on one line too, how?
-in_file = open(from_file)
-indata = in_file.read()
-
-print "The input file is %d bytes long" % len(indata)
+indata = open(from_file).read()
 
 print "Does the output file exist? %r" % exists(to_file)
-print "Ready, hit RETURN to continue, CTRL-C to abort."
-raw_input()
 
 out_file = open(to_file, 'w')
 out_file.write(indata)
@@ -24,3 +19,8 @@ print "Alright, all done."
 
 out_file.close()
 in_file.close()
+
+"""
+Study Drills
+2. Try to make it more friendly to use by removing features
+"""
